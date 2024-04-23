7e55a6ca3313545f51fd96367395b486b99b85ae
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-23 08:24:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ from pymongo.collection import Collection
 
 dotenv.load_dotenv(".env")
 
+__all__ = ['bot', 'gogo', 'api_id', 'api_hash', 'bot_token', 'db_url', 'database_name', 'owner_id', 'bot_username']
+
 api_id = os.environ.get('API_ID', '10471716')
 api_hash = os.environ.get('API_HASH', 'f8a1b21a13af154596e2ff5bed164860')
 bot_token = os.environ.get('BOT_TOKEN', '6999401413:AAHgF1ZpUsCT5MgWX1Wky7GbegyeHvzi2AU')
@@ -15,13 +17,12 @@ database_name = os.environ.get('DATABASE_NAME', 'Cluster0')
 owner_id = int(os.environ.get('OWNER_ID', '6883997969'))
 bot_username = os.environ.get('BOT_USERNAME', 'Io_TesterBot')
 
-def initialize():
-    bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
-    client = MongoClient(db_url, tls=True)
-    data = Collection(client[database_name], 'ConfigDB').find_one({"_id":"GogoAnime"})
-    gogo = Gogo(
+bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+client = MongoClient(db_url, tls=True)
+data = Collection(client[database_name], 'ConfigDB').find_one({"_id":"GogoAnime"})
+
+gogo = Gogo(
         gogoanime_token=data["gogoanime"],
         auth_token=data["auth"],
         host=data["url"]
     )
-    return bot, gogo
